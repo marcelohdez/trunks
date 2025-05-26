d6815009e56eb4c8a3cfa652496fc0a596d3fe97
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2025-05-26 14:51:37 -0400

    fix: geist fonts were not used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, DM_Serif_Text, Poppins } from "next/font/google";
+import { DM_Serif_Text, Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const dmSerifText = DM_Serif_Text({
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
 
 export const metadata: Metadata = {
   title: "trunks.dev",
   description: "tbd",
 };
+
+const dmSerifText = DM_Serif_Text({
+  variable: "--font-dm-serif-text",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function RootLayout({
   children,
